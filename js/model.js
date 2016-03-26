d0fa78320231ae4a9be7531a67e29a5e853edc5c
RyuAkiraYuki <rurounikenshinyuki@gmail.com>
2016-03-26 23:32:43 +0300

    updating model.js

--- js/model.js
+++ js/model.js
@@ -1,7 +1,28 @@
 GenericMVCJS.Model = (function ($) {
-    function Model() {
+    var callbackMissingMsg = 'There is no callback for you data update cenario. Don\'t forget to init it with SetUpdateCallbackMethod()';
+
+    function Model(updatecallback) {
+        this.Data = null;
+        this.UpdateCallback = updatecallback === undefined ? console.log(callbackMissingMsg): updatecallback;
+
+        $(this).on('updated', function (event) {
+            this.UpdateCallback();
+        })
 
     };
+
+    Model.prototype.SetUpdateCallbackMethod = function (callback) {
+        this.UpdateCallback = callback;
+    }
+
+    Model.prototype.SetData = function (data) {
+        this.Data = data;
+        $(this).trigger('updated');
+    }
+
+    Model.prototype.GetData = function () {
+        return this.Data;
+    }
 
     Model.prototype.Fetch = function (uri, callback) {
         $.get(uri).then(callback, function (result) {
